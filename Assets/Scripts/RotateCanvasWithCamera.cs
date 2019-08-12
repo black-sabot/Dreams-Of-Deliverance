84d80420665b5431598824a45b8496b699df1883
Velkas <treestump34@gmail.com>
2019-08-11 20:33:27 -0700

    Made player sprite canvas rotation follow player camera rotation

---
+++ Assets/Scripts/RotateCanvasWithCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCanvasWithCamera : MonoBehaviour
+{
+    private Camera camera;
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.rotation = camera.transform.rotation;
+    }
+}
