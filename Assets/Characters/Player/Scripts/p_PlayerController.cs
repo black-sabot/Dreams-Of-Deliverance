952cb12dd4916aa628227f7ade4b876a3dac66e4
Velkas <treestump34@gmail.com>
2019-08-11 23:44:45 -0700

    Added directional idle animations for stopping points, weak logic for capturing the last input to determine which idle to stop at.

--- Assets/Characters/Player/Scripts/p_PlayerController.cs
+++ Assets/Characters/Player/Scripts/p_PlayerController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class p_PlayerController : MonoBehaviour
 {
+    // Player movement speed
+    // TODO: add sprint?
     public float movementSpeed = 7f;
 
+    // Component tracking
     private Rigidbody rigidbody;
-    private Vector3 movement;
     private Animator animator;
+
+    // Movement tracking
+    private Vector3 lastIdle;
+    private Vector3 movement;
 
     void Start()
     {
+        // Get components needed at the start, no messy drag and drop
+        // Minimal impact on startup
         animator = GetComponentInChildren<Animator>();
         rigidbody = GetComponent<Rigidbody>();
+
+        // Set to 0 which is the default 'down' state for idle animation
+        // so character always starts looking at camera
+        lastIdle = Vector3.zero;
     }
 
     void Update()
@@ -23,15 +35,59 @@ public class p_PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
 
+        // Go store the last movement direction, normalized, for animation
+        GetLastDirectionInput(movement);
+
         // Set animator values
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.z);
-        animator.SetFloat("speed", movement.sqrMagnitude);
+        animator.SetFloat("lastX", lastIdle.x);
+        animator.SetFloat("lastZ", lastIdle.z);
+        animator.SetBool("moving", movement.sqrMagnitude != 0 ? true : false);
     }
 
     void FixedUpdate()
     {
         // Simple movement -- normalized for diagonal speed
         rigidbody.MovePosition(rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void GetLastDirectionInput(Vector3 movementInput)
+    {
+        // Left
+        if (movementInput.x < 0)
+        {
+            lastIdle.x = -1;
+        }
+
+        // Right
+        if (movementInput.x > 0)
+        {
+            lastIdle.x = 1;
+        }
+
+        // Up
+        if (movementInput.z < 0)
+        {
+            lastIdle.z = -1;
+        }
+
+        // Down
+        if (movementInput.z > 0)
+        {
+            lastIdle.z = 1;
+        }
+
+        // Clear z if only going left or right
+        if (movementInput.x != 0 && movementInput.z == 0)
+        {
+            lastIdle.z = 0;
+        }
+
+        // Clear x if only going up or down
+        if (movementInput.x == 0 && movementInput.z != 0)
+        {
+            lastIdle.x = 0;
+        }
     }
 }
