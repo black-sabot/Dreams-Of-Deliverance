485f5819054a8d679dce65a65723ce0ff305f7cc
Velkas <treestump34@gmail.com>
2019-08-11 18:31:56 -0700

    Simple movement
    Setup Win64 build

---
+++ Assets/Characters/Player/Scripts/p_PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p_PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    private Rigidbody rigidbody;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Capture input
+        // TODO: Build a better system for this so update doesn't get interrupted
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Simple movement
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
