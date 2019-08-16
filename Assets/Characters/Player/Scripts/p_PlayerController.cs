2f342baf365278bc975af6128426d1b1f55802f0
Lolzygag <lolzygag13@gmail.com>
2019-08-15 20:37:39 -0400

    Dodge roll mechanic basic implementation - also functions as a dash

--- Assets/Characters/Player/Scripts/p_PlayerController.cs
+++ Assets/Characters/Player/Scripts/p_PlayerController.cs
@@ -7,6 +7,11 @@ public class p_PlayerController : MonoBehaviour
     // Player movement speed
     // TODO: add sprint?
     public float movementSpeed = 7f;
+    public float rollSpeed;
+    private float rollTime;
+    public float startRollTime;
+    private int direction;
+    private bool isRolling = false;
 
     // Component tracking
     private Rigidbody rigidbody;
@@ -26,14 +31,40 @@ public class p_PlayerController : MonoBehaviour
         // Set to 0 which is the default 'down' state for idle animation
         // so character always starts looking at camera
         lastIdle = Vector3.zero;
+        rollTime = startRollTime;
     }
 
     void Update()
     {
         // Capture input
         // TODO: Build a better system for this so update doesn't get interrupted
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
+        if (!isRolling)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.z = Input.GetAxisRaw("Vertical");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isRolling)
+        {
+            isRolling = true;
+            Debug.Log("Rolling away");
+        }
+        
+        if(isRolling)
+        {
+            if (rollTime <= 0)
+            {
+                rollTime = startRollTime;
+                isRolling = false;
+            }
+            else
+            {
+                rollTime -= Time.deltaTime;
+
+                rigidbody.velocity = rigidbody.velocity * rollSpeed;
+            }
+        }
+
 
         // Go store the last movement direction, normalized, for animation
         GetLastDirectionInput(movement);
@@ -49,7 +80,13 @@ public class p_PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Simple movement -- normalized for diagonal speed
-        rigidbody.MovePosition(rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
+        if (isRolling)
+            if (movement == Vector3.zero)
+                rigidbody.MovePosition(rigidbody.position - lastIdle.normalized * rollSpeed/2 * Time.fixedDeltaTime);
+            else
+                rigidbody.MovePosition(rigidbody.position + lastIdle.normalized * rollSpeed * Time.fixedDeltaTime);
+        else
+            rigidbody.MovePosition(rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
     }
 
     void GetLastDirectionInput(Vector3 movementInput)
